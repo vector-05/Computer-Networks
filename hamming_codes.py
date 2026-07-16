952060b73800fd26f598bba2e6c82f79f8a154fc
Vector05 <mathurraghav23@gmail.com>
2026-07-16 18:17:44 +0000

    update1

--- hamming_codes.py
+++ hamming_codes.py
@@ -10,16 +10,21 @@ if __name__ == '__main__':
     # Data Self Correction Codes (Simulation)
     # Data Bit - n bit input data
 
-    d = input("Enter Data Bits: ")                  # Data Input
+    d = input("Enter Data: ")                  # Data Input
     data_bits = len(d)                                 # Data Input Length
 
-    parity_bits = 0                                 # initialize number of parity bits
+    parity_bits = 0                                 # number of parity bits
     while True:
         if (2 ** parity_bits) >= (parity_bits + data_bits + 1):
             break
-        parity_bits++
+        parity_bits += 1
 
-    code_bits = data_bits + parity_bits
+    code_bits = data_bits + parity_bits     # sender's code
+    
+    data = list[d]                  # data bits array
+    parity = [2] * parity_bits      # parity bits array
+
+    code = []
     
 
 
